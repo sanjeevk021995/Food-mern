9e807b250a99a60637cc421a81bd933428aa8b8a
sanjeevk021995 <sanjeev.kush95@gmail.com>
2024-05-19 13:41:14 +0530

    added react-router-dom

--- README.md
+++
@@ -1,3 +0,0 @@
-shortcut
-for js snippet for react
-for selecting multiple occurence and replacinf the word ctrl shif L
--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
-import './App.css';
-
+import './App.css'
 import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <>
-      <div> <Home/> </div>
-    </>
+    <Router>
+      <div>
+        <Routes>  
+          <Route exact path="/" element={<Home/>}/>
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
